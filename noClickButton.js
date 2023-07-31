b2db7502b7deddd187077cd32826dba6e9181ad0
juSt6790 <120416303+juSt6790@users.noreply.github.com>
2023-07-31 22:25:38 +0530

    Update noClickButton.js

--- noClickButton.js
+++ noClickButton.js
@@ -4,6 +4,7 @@ function loff() {
     const ques = document.querySelector(".question");
     const button = document.querySelector(".buttons");
     ques.style.transition = '1s';
-    ques.innerHTML = "Allah Will Punish You";
+    ques.style.color = "red";
+    ques.innerHTML = "<h1>Allah Will Punish You<br><br>Your Dick Will<br>Regenarate Soon</h1>";
     button.innerHTML = "";
 }
